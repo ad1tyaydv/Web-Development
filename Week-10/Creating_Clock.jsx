59006ee78111841905c985556c02c63ed27f59bb
Aditya Yadav <adityaydv12890@gmail.com>
2025-05-30 22:19:07 +0530

    Create Creating_Clock.jsx

---
+++ Week-10/Creating_Clock.jsx
@@ -0,0 +1,29 @@
+import { useRef, useState } from 'react'
+
+function App() {
+  const [currentCount, setCurrentCount] = useState(1);
+  const timer = useRef();
+  
+  function startClock() {
+    let value = setInterval(function() {
+      setCurrentCount(function(count) {
+        return count + 1;
+      });
+      }, 1000);
+      timer.current = value;
+    }
+
+    function stopClock() {
+      clearInterval(timer.current);
+    }
+
+
+  return <div>
+    {currentCount}
+    <br />
+    <button onClick={startClock}>Start</button>
+    <button onClick={stopClock}>Stop</button>
+  </div>
+}
+
+export default App
