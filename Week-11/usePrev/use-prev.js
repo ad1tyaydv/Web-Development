958c34a76c230c7f90a6919cee86e68365772ea2
Aditya Yadav <adityaydv12890@gmail.com>
2025-06-05 23:52:44 +0530

    Create use-prev.js

---
+++ Week-11/usePrev/use-prev.js
@@ -0,0 +1,11 @@
+import { useEffect, useRef, useState } from "react";
+
+export const usePrev = (value) => {
+    const ref = useRef();
+
+    useEffect(() => {
+        ref.current = value;
+    }, [value]);
+
+    return ref.current;
+}
