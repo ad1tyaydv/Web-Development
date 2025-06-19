5d09b638483fec26ef61a954ca36722a1e1cc3dd
Aditya Yadav <adityaydv12890@gmail.com>
2025-06-19 19:12:40 +0530

    Create App.jsx

---
+++ Week-13-Tailwind/OTP-buttons/App.jsx
@@ -0,0 +1,16 @@
+import { Otp } from "./components/Otp";
+
+
+function App() {
+
+  return (
+    <div className="h-screen bg-[#00264c]">
+        <div>
+          <br /> <br /> <br /> <br /> <br /> <br />
+          <Otp />
+        </div>
+    </div>
+  );
+}
+
+export default App
