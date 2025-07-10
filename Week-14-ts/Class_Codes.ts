53398e8e42877a1c4d36f91095f259fd9adcce8f
Aditya Yadav <adityaydv12890@gmail.com>
2025-07-10 17:04:14 +0530

    Update Class_Codes.ts

--- Week-14-ts/Class_Codes.ts
+++ Week-14-ts/Class_Codes.ts
@@ -23,3 +23,30 @@ isLegal({
     age: 20,
     email: "ADITYAYADAV"
 })
+
+-----------------------------------------------------------------------------------------------------------
+
+    interface Person {
+    firstName: String;
+    lastName: String;
+    age: number;
+    greet(phrase: String): void;
+};
+
+
+class Employee implements Person {
+    name: String;
+    age: number;
+
+    constructor(n: String, a: number) {
+        this.name = n;
+        this.age = a;
+    }
+
+    greet(pharse: String) {
+        console.log(`${pharse} ${this.name}`);
+    }
+}
+
+const e = new Employee("Aditya", 22);
+console.log(e.name);
